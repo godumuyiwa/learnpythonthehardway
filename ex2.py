941b81db5f8f40733cb31e12b4f48f4560430903
godumuyiwa <ishaqgbo@gmail.com>
2018-12-28 10:17:46 +0000

    Day 1

---
+++ ex2.py
@@ -0,0 +1,9 @@
+# A comment, this so you can read your program later
+# Anything after the # is ignored python
+
+print("I could have code like this.") # the comment after this is ignored
+
+#you can also use a comment to "disable or comment out code"
+# print("This wont run")
+
+print("This will run.")
